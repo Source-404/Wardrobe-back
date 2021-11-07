22eadd350c26aef303b9efe7ddbc9a442bb36aa6
Source-404 <suyashjohri3@gmail.com>
2021-11-08 04:09:22 +0530

    removed boiler code

--- db.js
+++ db.js
@@ -1,13 +1,5 @@
 const Pool = require("pg").Pool;
 
-// const pool = new Pool ({
-//     user: "postgres",
-//     password: "ujjwal",
-//     host : "localhost",
-//     port : 5432,
-//     database : "bug_tracking"
-// });
-
 const pool = new Pool({
   connectionString:
     "postgresql://uoxhlwogxay5v5apuy56:VRz8IpV2CsZPXxySlGLK@bmsoc22spurxnysqdrf8-postgresql.services.clever-cloud.com:5432/bmsoc22spurxnysqdrf8",
